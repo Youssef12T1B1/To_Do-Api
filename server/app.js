91320bba8f7f4f6a5872dd96a874efc56e1cc9d6
Youssef12T1B1 <yousseattabi1998@gmail.com>
2022-03-22 14:52:03 +0100

    Api

--- server/app.js
+++ server/app.js
@@ -11,6 +11,23 @@ app.use(express.json());
 app.get("/", (req, res, next) => {
   res.json({ message: "server is running" });
 });
+
 app.use("/todo", TodoRouter);
+app.use("/user", UserRouter);
+
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
 
 app.listen(PORT, () => console.log(`Server running At ${PORT}`));
--- server/routes/user.js
+++ server/routes/user.js
@@ -4,3 +4,105 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const JwtSecret = require("../config/.env").JWT_SEC;
+const ensureAuthenticated = require("../auth/ensureAuthenticated");
+
+router.post("/register", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user) {
+        res.status(409).json({
+          message: "Email Already Used",
+        });
+      } else {
+        bcrypt.hash(req.body.password, 12, (err, hash) => {
+          if (err) {
+            return res.status(500).json({
+              error: err,
+            });
+          } else {
+            const user = new User({
+              username: req.body.username,
+              email: req.body.email,
+              password: hash,
+            });
+            user
+              .save()
+              .then((result) => {
+                res.status(201).json({
+                  message: "user created Successfully",
+                });
+              })
+              .catch((err) => {
+                res.status(400).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    });
+});
+
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      bcrypt.compare(req.body.password, user.password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth Failed",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user.email,
+              userId: user._id,
+            },
+            JwtSecret,
+            { expiresIn: "1h" }
+          );
+          return res.status(200).json({
+            message: "Login Successfully",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth Failed",
+        });
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+router.delete("/:id", ensureAuthenticated, (req, res, next) => {
+  const id = req.params.id;
+  User.deleteOne({ _id: id })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "User Deleted successfully",
+        request: {
+          type: "POST",
+          url: "http://localhost:4000/user",
+          body: {
+            email: "Email",
+            password: "String",
+          },
+        },
+      });
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "no user with this id: " + id,
+        error: err,
+      });
+    });
+});
+
+module.exports = router;
